23e5a4a22e3d117aac6581c46906c84f89620b18
Son2tq <sonnvph19457@fpt.edu.vn>
2022-11-28 02:23:18 +0700

    [#13] Blog và thêm vào cái mới

--- admin/views/blocks/sidebar.php
+++ admin/views/blocks/sidebar.php
@@ -12,7 +12,7 @@
 	<!--navigation-->
 	<ul class="metismenu" id="menu">
 		<li>
-			<a href="javascript:;" class="has-arrow">
+			<a href="index.php?controller=home" class="has-arrow">
 				<div class="parent-icon"><i class="bx bx-home-alt"></i>
 				</div>
 				<div class="menu-title">Dashboard</div>
